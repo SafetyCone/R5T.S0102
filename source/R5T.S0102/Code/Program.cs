802be9d4a64f667c79334fab4d8117223d0a83d8
davidcoats <david@getsafetycone.com>
2023-11-05 11:52:52 -0800

    Interim changes.

--- source/R5T.S0102/Code/Program.cs
+++ source/R5T.S0102/Code/Program.cs
@@ -21,8 +21,8 @@ namespace R5T.S0102
             //await Scripts.Instance.Get_RivetIdentityNames();
             //await Scripts.Instance.RoundTripParse_IdentityNames();
             //await Scripts.Instance.GenerateAndCheck_DotnetPackMemberIdentityStrings();
-            //Scripts.Instance.GenerateAndCheck_DotnetPackMemberIdentityStrings_FromSignatures();
-            await Scripts.Instance.GenerateAndCheck_CodebaseIdentityStrings_FromSignatures();
+            Scripts.Instance.GenerateAndCheck_DotnetPackMemberIdentityStrings_FromSignatures();
+            //await Scripts.Instance.GenerateAndCheck_CodebaseIdentityStrings_FromSignatures();
 
             //Demonstrations.Instance.Is_MinimallyValidIdentityName();
             //Demonstrations.Instance.Get_ArgumentsList();
